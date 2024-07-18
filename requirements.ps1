af273ca480e6b4c9c3e239505be68eb3df4d5e69
flamendO <78015005+flamendO@users.noreply.github.com>
2024-07-18 15:38:02 +0200

    Update requirements.ps1

--- requirements.ps1
+++ requirements.ps1
@@ -9,16 +9,16 @@ pip install urllib3
 
 (Get-Content -Path "./src/dist/flags.txt" -Raw) -replace "(?m)^(.*\r?\n)(.*)$", "`${1}1" | Set-Content -Path "./src/dist/flags.txt"
 
-# Chemin absolu complet vers l'exécutable
+
 $target = "$PWD\src\dist\gui\gui.exe"
 
-# Chemin absolu complet vers le fichier de raccourci avec extension .lnk
+
 $shortcutPath = "$PWD\Man-Of-Steal.lnk"
 
-# Créer un objet pour créer le raccourci
+
 $shell = New-Object -ComObject WScript.Shell
 
-# Créer le raccourci
+
 $shortcut = $shell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = $target
 $shortcut.Description = "Shortcut"
